9bc2b52c65b16a4b6de20dcf7f085ccbf4f623d2
Sint Sint Hlaing <sints2001@gmail.com>
2026-06-29 15:49:30 +0630

    Add Entity

---
+++ serenemind-api/src/main/java/com/mental/model/entity/Notification.java
@@ -0,0 +1,22 @@
+package com.mental.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "notifications")
+@Getter
+@Setter
+public class Notification extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    private String title;
+    private String message;
+    private boolean isRead;
+}
